b226a811aec84c23c59e2f0a77b393875d631cdf
jonattfin <jonattfin@outlook.com>
2022-12-08 16:17:10 +0200

    small hanges

--- Ecosia.SearchEngine/Ecosia.SearchEngine.Api/Extensions/DistributedCacheExtensions.cs
+++ Ecosia.SearchEngine/Ecosia.SearchEngine.Api/Extensions/DistributedCacheExtensions.cs
@@ -5,14 +5,27 @@ namespace Ecosia.SearchEngine.Api.Extensions;
 
 public static class DistributedCacheExtensions
 {
+    private static readonly bool _useCache = false;
+    
     public static async Task<T?> GetObjectAsync<T>(this IDistributedCache cache, string key)
     {
+        if (!_useCache)
+        {
+            return await Task.FromResult(default(T));
+        }
+        
         var value = await cache.GetStringAsync(key);
         return value == null ? default : JsonConvert.DeserializeObject<T>(value);
     }
 
     public static async Task SetObjectAsync<T>(this IDistributedCache cache, string key, T value)
     {
+        if (!_useCache)
+        {
+            await Task.FromResult(default(T));
+            return;
+        }
+        
         await cache.SetStringAsync(key, JsonConvert.SerializeObject(value),
             new DistributedCacheEntryOptions() { AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(10) });
     }
